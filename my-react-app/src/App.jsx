7e9d514773660e533510bc3f2ad9043f5ceb2755
SahoryHerrera <sahory0920@gmail.com>
2024-06-11 19:36:44 -0600

    Agregue modulos

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import LaptopsModal from './components/laptops-modal/laptopsModal';
+import AppContent from './components/App/AppContent';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          boton: {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Hecho por Sahory
-      </p>
-    </>
-  )
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/laptops-modal">Laptops Modal</Link>
+          </li>
+          <li>
+            <Link to="/app-content">App Content</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/laptops-modal" element={<LaptopsModal />} />
+        <Route path="/app-content" element={<AppContent isOpen={true} onClose={() => {}} />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
